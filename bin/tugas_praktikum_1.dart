e54d50c4c4da201af31bfa4b04c2f2ced14217eb
Zaki Lazuardi Ferysa Putra <zakilfp987@gmail.com>
2024-09-02 10:05:52 +0700

    Laporan 1

---
+++ bin/tugas_praktikum_1.dart
@@ -0,0 +1,7 @@
+// import 'package:tugas_praktikum_1/tugas_praktikum_1.dart' as tugas_praktikum_1;
+
+void main() { 
+   for (int i = 18; i >= 9; i--) { 
+    print('Nama saya adalah Fulan, sekarang berumur $i'); 
+  } 
+}
---
+++ laporan p1/Laporan.md
@@ -0,0 +1,7 @@
+# Laporan Praktikum Pemrograman Mobile
+
+Nama    : Zaki Lazuardi Ferysa Putra
+Kelas   : TI-3B / 27
+
+## Tugas Praktikum
+![alt text](<Screenshot 2024-09-02 084535.png>)
